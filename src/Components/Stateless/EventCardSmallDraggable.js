056b59861e6c68d617b0fbdc0d4f3bac564f9323
Audrius Petrauskas <audrius.j.petrauskas@gmail.com>
2022-04-26 15:28:47 +0100

    Fix font size for creation screen

--- src/Components/Stateless/EventCardSmallDraggable.js
+++ src/Components/Stateless/EventCardSmallDraggable.js
@@ -4,6 +4,32 @@ import { Draggable } from 'react-beautiful-dnd';
 export default EventCardSmallDraggable;
 
 function EventCardSmallDraggable({ description, imageSrc, date, id, index }) {
+    function GetWordApproxCount(description) {
+        return description.split(" ").length - 1;
+    }
+
+    if (description && GetWordApproxCount(description) > 3) {
+        return (
+            <div style={cardSeparation} key={id}>
+                <Draggable draggableId={id} index={index}>
+                    {(provided) => (
+
+                        <Card {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}
+                            style={getEventStyle(provided.draggableProps.style)}>
+                            <Card.Img variant="left" src={imageSrc} style={imageStyle} />
+                            <Card.Body>
+                                <Card.Title style={{ fontSize: "15px" }}>{description}</Card.Title>
+                                <Card.Text>
+                                    {date}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    )}
+
+                </Draggable>
+            </div>
+        )
+    }
 
     return (
         <div style={cardSeparation} key={id}>
